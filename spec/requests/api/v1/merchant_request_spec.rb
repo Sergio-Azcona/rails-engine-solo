4b9a4df30f38d7b8586a95cadcb57ac58da9ba14
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-02-07 22:39:00 -0500

    test/feat: show complete

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -6,7 +6,6 @@ describe "Merchants API" do
     create_list(:merchant, 5)
 
     get '/api/v1/merchant'
-
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
@@ -23,5 +22,20 @@ describe "Merchants API" do
     end
   end
 
+  it 'can get a single/show merchant' do
+    merchant = create_list(:merchant,1).first
+    
+    get "/api/v1/merchant/#{merchant.id}"
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+      # require 'pry';binding.pry
+      require 'pry';binding.pry if merchant[:id] == false
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_an(Integer)
+      
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
 
 end
